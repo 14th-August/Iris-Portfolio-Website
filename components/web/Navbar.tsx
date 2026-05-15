8b0735b24f70d422d04bd7cebfb15261fa08ba30
14th-August <cs.cfadams@gmail.com>
2026-05-15 14:50:34 -0700

    Added an event listener to return use state to closed whenever we resize window.

--- components/web/Navbar.tsx
+++ components/web/Navbar.tsx
@@ -1,42 +1,61 @@
 "use client";
 
 import Link from 'next/link';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function Navbar() {
 
   const [isOpen, setIsOpen] = useState(false);
 
-  function getMenuClasses() {
+  // Added the resize listener to reset mobile state when returning to full screen
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768 && isOpen) {
+        setIsOpen(false);
+      }
+    };
 
-    let menuClasses = [];
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [isOpen]);
+
+  function getMenuClasses() {
+    let menuClasses = [
+      'md:flex',
+      'md:flex-row',
+      'md:static',         
+      'md:w-auto',         
+      'md:bg-transparent', 
+      'md:p-0',            
+      'md:gap-10',
+      'md:items-center'
+    ];
 
     if (isOpen) {
-      menuClasses = [
+      menuClasses.push(
         'flex',
         'flex-col',
         'items-start',
         'absolute',
-        'top-[60px]',
+        'top-[50px]',
         'bg-gray-600',
         'w-full',
         'p-4',
         'left-0',
-        'gap-10',
-      ];
+        'gap-10'
+      );
     } else {
-      menuClasses = ["hidden", "md:flex"];
+      menuClasses.push("hidden");
     }
 
     return menuClasses.join(' ');
-  
   }
 
   return (
     <nav className="bg-gray-600 text-black p-4 sm:p-6 md:flex md:justify-between md:items-center">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="font-[family-name:var(--font-montserrat)] font-extralight uppercase
-        tracking-[0.3em] txt-xl md:text-1xl text-white">
+          tracking-[0.3em] text-xl md:text-xl text-white">
           Iris
         </Link>
         <div className={getMenuClasses()}>
@@ -54,7 +73,7 @@ export function Navbar() {
             }}>
               {isOpen ? (
                 <svg 
-                  className="w-6 h-6" 
+                  className="w-6 h-6 text-white" 
                   fill="none" 
                   stroke="currentColor" 
                   viewBox="0 0 24 24" 
@@ -64,7 +83,7 @@ export function Navbar() {
                 </svg>
               ) : (
                 <svg 
-                  className="w-6 h-6" 
+                  className="w-6 h-6 text-white" 
                   fill="none" 
                   stroke="currentColor" 
                   viewBox="0 0 24 24" 
