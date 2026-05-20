c7e6dcb17a5a88a9b11220691526ad632f1ae0fd
14th-August <cs.cfadams@gmail.com>
2026-05-19 23:17:40 -0700

    Felt more like chilling today, just made the IntroCard component and thats it

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import VideoHero from "../components/ui/Video";
 import Displays from "../components/ui/Displays";
 import { ImageDisplayData } from "../images/portfolioImages";
+import Introcard from "../components/ui/Introcard";
 export default function Home() {
   return (
     <>
       <VideoHero />
       <Displays images={ImageDisplayData} />
+      <Introcard />
     </>
   );
 }
---
+++ components/ui/Introcard.tsx
@@ -0,0 +1,9 @@
+export default function IntroCard() {
+        return (
+            <div className="relative flex-none w-[80vw] md:w-96 aspect-[4/5] group select-none">
+                <div className="absolute inset-0 overflow-hidden bg-gray-900">
+                    Text
+                </div>
+            </div>
+        );
+} 
